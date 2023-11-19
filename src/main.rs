43329c94fdb4ed54f191b8842fd964908c02b102
taras-sereda <taras.y.sereda@proton.me>
2023-11-19 17:56:02 +0200

    expicit parallelization with rayon

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,16 @@ parallel -0 ffprobe -hide_banner -v error -of  -show_entries stream=duration  14
 paste - -sd+ -  0.01s user 0.03s system 0% cpu 29.345 total
 bc  0.00s user 0.00s system 0% cpu 29.346 total
  */
+
 use clap::Parser;
 use glob::glob;
 use hound;
+use rayon::prelude::*;
+use rayon::{current_num_threads, current_thread_index};
 use std::fs::File;
+use std::path::Path;
 use std::time::{Duration, Instant};
+use std::{thread, time};
 
 #[derive(Parser)]
 struct Cli {
@@ -28,8 +33,24 @@ fn wav_duration(f_name: &String) -> f32 {
     let duration = reader.duration();
     let dur_sec = duration as f32 / spec.sample_rate as f32;
 
-    //println!("duration: {:?} sec", dur_sec);
-    return dur_sec;
+    dur_sec
+}
+
+fn wav_duration2<AR>(f_name: AR) -> f32
+where
+    AR: AsRef<Path>,
+{
+    //let sleep_dur = time::Duration::from_millis(1000);
+    //thread::sleep(sleep_dur);
+    let th_idx = current_thread_index().unwrap();
+    println!("{}", th_idx);
+    let f = File::open(f_name.as_ref()).unwrap();
+    let reader = hound::WavReader::new(f).unwrap();
+    let spec = reader.spec();
+    let duration = reader.duration();
+    let dur_sec = duration as f32 / spec.sample_rate as f32;
+
+    dur_sec
 }
 
 fn wav_size(f_name: &String) {
@@ -38,25 +59,17 @@ fn wav_size(f_name: &String) {
     let size = hound::read_wave_header(&mut f).unwrap();
     println!("size: {:?}", size);
 }
-fn main() {
+fn main() -> Result<(), glob::PatternError> {
     let start = Instant::now();
     let args = Cli::parse();
+    let global_num_threads = current_num_threads();
+    println!("num threads: {}", global_num_threads);
 
     let glob_pattern = args.path.clone().into_os_string().into_string().unwrap() + "/**/*.wav";
-
-    //let glob_pattern = args.path.to_str().unwrap().to_owned() + "/**/*.wav";
-    let mut total_dur = 0.0;
-    let mut num_entires = 0;
-    for entry in glob(&glob_pattern).expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => {
-                let dur = wav_duration(&path.into_os_string().into_string().unwrap());
-                total_dur += dur;
-                num_entires += 1;
-            }
-            Err(e) => println!("{:?}", e),
-        }
-    }
+    let entries: Vec<_> = glob(&glob_pattern)?.filter_map(|path| path.ok()).collect();
+    let durations: Vec<_> = entries.par_iter().map(|path| wav_duration2(path)).collect();
+    let total_dur: f32 = durations.iter().sum();
+    let num_entires = durations.len();
 
     println!("Calucalting total duration for directory: {:?}", args.path);
     println!("Number of wav files: {}", num_entires);
@@ -64,4 +77,5 @@ fn main() {
     println!("Total duration: {} seconds", total_dur);
     let exec_duration = start.elapsed();
     println!("Executed in {:?}", exec_duration);
+    Ok(())
 }
